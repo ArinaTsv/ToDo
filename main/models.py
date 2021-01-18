1849c5a3033b99e1ad9f4ca7e2a16809f318f13c
ArinaTsventarnaya <a.tsventarnaya@gmail.com>
2021-01-18 14:49:53 +0600

    bookstore project
    -  created book model;,
    - added migrations to db;
    - added books.html;

--- main/models.py
+++ main/models.py
@@ -5,3 +5,13 @@ class ToDo(models.Model):
     created_at = models.DateField(auto_now_add=True)
     is_closed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
+
+class Books(models.Model):
+    title = models.CharField(max_length=50)
+    subtitle = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.IntegerField()
+    genre = models.CharField(max_length=20)
+    author = models.CharField(max_length=30)
+    year = models.IntegerField()
+    date = models.DateField(auto_now_add=True)
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from .models import ToDo
+from .models import ToDo, Books
 
 def homepage(request):
     return render(request, "index.html")
@@ -7,5 +7,8 @@ def homepage(request):
 
 def test(request):
     todo_list = ToDo.objects.all()
-    print (todo_list)
-    return render(request, "test.html", {"todo_list": todo_list})
+    return render(request, "test.html", {"todos": todo_list})
+
+def books(request):
+    book_list = Books.objects.all()
+    return render(request, "books.html", {"books": book_list})
